f851c69053f11a0b33d3add67af31214cf374a80
SofianeMrb <136330945+SofianeMrb@users.noreply.github.com>
2024-02-01 15:43:23 +0100

    feat : added a result popup

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,18 @@
 import {useState , useRef} from 'react';
+import ResultModal from './resultModal.jsx';
+
 
 
 export default function TimerChallenge({title, targetTime}){
     const timer = useRef();
+    const dialog = useRef();
 const [timerStarted, setTimerStarted] = useState(false);
 const [timerExpired, setTimerExpired] = useState(false);
 
 function handleStart(){
   timer.current = setTimeout(() => {
     setTimerExpired(true);
+    dialog.current.open();
 }, targetTime * 1000);
 
 setTimerStarted(true);
@@ -18,9 +22,11 @@ function handleStop(){
     clearTimeout(timer.current)
 }
 
-    return <section className="challenge">
+    return ( <>
+
+    <ResultModal ref={dialog} targetTime={targetTime} result='lost' />
+    <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>You Lost!</p>}
         <p className="challenge-timer">
             {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
@@ -31,4 +37,6 @@ function handleStop(){
           {timerStarted ? 'Time is Running' : 'Time is Stopped'}  
         </p>
     </section>
+    </>
+    )
 }
